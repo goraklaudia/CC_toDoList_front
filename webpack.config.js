4c1dacde70b2a7bd9be293befcc723dd5360dc4b
Kamil Żogło <kamilzoglo@gmail.com>
2019-12-22 14:01:42 +0100

    Few alterations to make webpack work properly - good, but not enough.

---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              ['@babel/plugin-transform-runtime'],
+              ['@babel/plugin-transform-regenerator']
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.mst$/,
+        use: [
+          {
+            loader: 'mustache-loader'
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    //user
+    new HtmlWebpackPlugin({
+      template: './src/modules/users/templates/login.mst',
+      filename: 'login.mst'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/modules/users/templates/register.mst',
+      filename: 'register.mst'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/modules/users/templates/showDetails.mst',
+      filename: 'showDetails.mst'
+    }),
+    //tasks
+    new HtmlWebpackPlugin({
+      template: './src/modules/tasks/templates/addTask/addTask.mst',
+      filename: 'addTask.mst'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/modules/tasks/templates/tasksList/tasksList.mst',
+      filename: 'tasksList.mst'
+    }),
+    //projects
+    new HtmlWebpackPlugin({
+      template: './src/modules/projects/templates/addProject.mst',
+      filename: 'addProject.mst'
+    }),
+    //index
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devServer: {
+    port: 9000
+  }
+};
